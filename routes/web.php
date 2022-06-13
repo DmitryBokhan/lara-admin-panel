7987a068373ea2f431af495617f6fa9cfeae9e5d
Dmitry <iteushm@gmail.com>
2022-06-13 13:13:07 +0300

    Добавил скрипт
    Добавлен скрипт для индикации меню

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 //маршруты для админа
 Route::middleware(['role:admin'])->prefix('admin_panel')->group( function () {
-    Route::get('/', [HomeController::class, 'index']);
+    Route::get('/', [HomeController::class, 'index'])->name('homeAdmin');
 });
 
 
